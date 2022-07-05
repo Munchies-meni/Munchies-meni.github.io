3cb86cb444bf02585eeeb61ddf148be919e1d83a
Munchies-meni <107650797+Munchies-meni@users.noreply.github.com>
2022-07-05 12:33:16 +0200

    Bukvalno sve redoao
    poredao jedno jelo po ekranu i napravio tranzicije

--- js.js
+++ js.js
@@ -1,48 +1,13 @@
-let galerija = document.querySelectorAll("#galerija figure")
-console.log(galerija.nodes)
+let jela = document.querySelectorAll(".jelo")
 
-for (let i = 0; i < galerija.length; i++) {
-    galerija[i].addEventListener("click",(e) => {
-        var target = e.target;
-        target.parentElement.classList.toggle("oof")
+const observer = new IntersectionObserver( entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+},{
+    threshold : 0.35
+})
 
-        /*switch (target.parentElement.id) {
-            case "jelo1":
-                console.log("prvo");
-                target.parentElement.classList.toggle("oof")
-                break;
-            case "jelo2":
-                console.log("drugo");
-                target.parentElement.classList.toggle("oof")
-                break;
-            case "jelo3":
-                console.log("3");
-                target.parentElement.classList.toggle("oof")
-                break;
-        case "jelo4":
-                console.log("pr4vo");
-                target.parentElement.classList.toggle("oof")
-                break;
-            case "jelo5":
-                console.log("prv5o");
-                target.parentElement.classList.toggle("oof")
-                break;
-            case "jelo6":
-                console.log("prvo6");
-                target.parentElement.classList.toggle("oof")
-                break;
-            case "jelo7":
-                console.log("prv7");
-                target.parentElement.classList.toggle("oof")
-                break;
-            case "jelo8":
-                console.log("prv8");
-                target.parentElement.classList.toggle("oof")
-                break;
-            case "jelo9":
-                console.log("prv9");
-                target.parentElement.classList.toggle("oof")
-                break;
-        }*/
-    }, false)
-}
+jela.forEach(jelo => {
+    observer.observe(jelo)
+})
